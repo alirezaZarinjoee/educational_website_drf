877f168e80076bee5fe795d17944eb00940223b8
alirezaZarinjoee <alireza.zarinjoee.2004@gmail.com>
2024-05-17 08:59:08 -0700

    add model comment

--- apps/comment/models.py
+++ apps/comment/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from apps.product.models import Education
+from apps.account.models import CustomUser
 
-# Create your models here.
+
+class Comment(models.Model):
+    education=models.ForeignKey(Education, on_delete=models.CASCADE,related_name='comments')
+    user=models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name='comments_of_user')
+    text=models.TextField()
+    is_active=models.BooleanField(default=True)
+    register_date=models.DateTimeField(auto_now_add=True)
+    parent=models.ForeignKey('Comment', on_delete=models.CASCADE,related_name='comments_child')
+    
+    def __str__(self):
+        return f'{self.education} - {self.user} - {self.text}'
+    
+    class Meta:
+        verbose_name='comment'
+        verbose_name_plural='comments'
+        
+ 
